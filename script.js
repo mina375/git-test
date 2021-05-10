934b40cebf3c7b500216d190bc227a9834457221
mina375 <mo37.moimoi@gmail.com>
2021-05-10 09:22:35 +0900

    1st js commit

---
+++ script.js
@@ -0,0 +1,17 @@
+
+$(function(){
+  //ハンバーガーメニュー
+  $('.burger-btn').on('click',function(){
+    $('.burger-btn').toggleClass('close');
+    $('.nav-list').toggleClass('slide-in');
+    $('body').toggleClass('noscroll');
+
+    if( $(window).width() < 600 ){//デバイスの幅が768以下のとき
+      $('.nav-item>a').on('click',function(){//.nav-item>aをクリックすると
+      $('.nav-list').removeClass('slide-in');//.nav-wrapperが0.5秒でフェードアウト(メニューのフェードアウト)
+      $('.burger-btn').removeClass('close');//.btn_triggerのcloseクラスを削除
+      $('body').removeClass('noscroll');//bodyのnoscrollクラスを削除
+    });
+  };
+  });
+});
